48f8abd9622a39cf7ddb41cb6353d69b90d7fee7
Salahub (WSL) <cdsalahub@gmail.com>
2022-02-08 11:59:27 -0500

    initialize

---
+++ auto/genMod.el
@@ -0,0 +1,146 @@
+(TeX-add-style-hook
+ "genMod"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("graphicx" "pdftex") ("natbib" "round") ("hyperref" "pdftex" "pagebackref=false")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "ifthen"
+    "amsmath"
+    "amssymb"
+    "amstext"
+    "graphicx"
+    "amsthm"
+    "amsfonts"
+    "dsfont"
+    "caption"
+    "color"
+    "dcolumn"
+    "bm"
+    "float"
+    "multirow"
+    "natbib"
+    "algorithm"
+    "algorithmicx"
+    "algpseudocode"
+    "hyperref")
+   (TeX-add-symbols
+    '("TODO" 1)
+    '("comment" 1)
+    '("needtocite" 1)
+    '("field" 1)
+    '("ind" 2)
+    '("bigwig" 1)
+    '("wig" 1)
+    '("widebar" 1)
+    '("stripe" 2)
+    '("oneblock" 3)
+    '("abs" 1)
+    '("frob" 1)
+    '("norm" 1)
+    '("conj" 1)
+    '("tr" 1)
+    '("sm" 1)
+    '("m" 1)
+    '("sv" 1)
+    '("ve" 1)
+    '("Desc" 2)
+    '("pkgsp" 1)
+    '("pkg" 1)
+    '("code" 1)
+    '("href" 1)
+    '("cmmd" 1)
+    '("package" 1)
+    "Rnsp"
+    "R"
+    "Pythonnsp"
+    "Python"
+    "mvec"
+    "trace"
+    "rank"
+    "diag"
+    "vspan"
+    "rowsp"
+    "colsp"
+    "svd"
+    "edm"
+    "abdiff"
+    "follows"
+    "leftgiven"
+    "given"
+    "biggiven"
+    "indep"
+    "notindep"
+    "depend"
+    "notdepend"
+    "imply"
+    "notimply"
+    "xyAssociation"
+    "xDomain"
+    "yDomain"
+    "measureRange"
+    "bigChi"
+    "mutInf"
+    "Had"
+    "measureAssociation"
+    "intersect"
+    "union"
+    "Reals"
+    "Integers"
+    "Naturals"
+    "Complex"
+    "Rationals"
+    "oldemptyset"
+    "emptyset")
+   (LaTeX-add-labels
+    "eq:indicator"
+    "fig:modelDiagram"
+    "subsec:crossingover"
+    "subsubsec:modelcrossing"
+    "eq:crossindicator"
+    "subsubsec:simplify"
+    "eq:poissonlim"
+    "eq:haldanemap"
+    "subsec:correlation"
+    "eq:z1var"
+    "eq:z2var"
+    "eq:covstep1"
+    "eq:covstep2"
+    "eq:covstep3"
+    "eq:cov"
+    "eq:precorr"
+    "eq:corrdist"
+    "tab:gamma"
+    "eq:zcorr"
+    "subsec:sim"
+    "fig:backsim"
+    "fig:intersim"
+    "subsec:model2real"
+    "fig:corr2real"
+    "fig:lineup"
+    "sec:apply"
+    "tab:r2"
+    "subsec:inlit"
+    "eq:rsq")
+   (LaTeX-add-bibliographies
+    "../Bibliography/fullbib")
+   (LaTeX-add-amsthm-newtheorems
+    "definition"
+    "theorem")
+   (LaTeX-add-color-definecolors
+    "background-color"
+    "steelblue"
+    "brickred"
+    "bluegray"
+    "amethyst"))
+ :latex)
+
