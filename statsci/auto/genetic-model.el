c18368054a5859fbb58ac9d7db905660ba518827
Salahub (WSL) <cdsalahub@gmail.com>
2022-05-20 11:31:33 -0400

    Updated stat sci article

--- statsci/auto/genetic-model.el
+++ statsci/auto/genetic-model.el
@@ -5,12 +5,12 @@
                      '(("imsart" "sts")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("natbib" "numbers") ("hyperref" "colorlinks" "citecolor=blue" "urlcolor=blue")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -61,11 +61,6 @@
     "measureRange"
     "bigChi"
     "mutInf"
-    "lmin"
-    "argmin"
-    "argmax"
-    "arginf"
-    "argsup"
     "Reals"
     "Integers"
     "Naturals"
